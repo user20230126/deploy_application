9d02fad9ff33b4e5efdf73eb8ebc32c676d0ca1f
user20230126 <1228mon0224wed@gmail.com>
2024-09-24 15:58:21 +0900

    修正

--- db/mysql_setup.sql
+++ db/mysql_setup.sql
@@ -1,4 +1,3 @@
-
-CREATE DATABASE IF NOT EXISTS daily_report_syste CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-CREATE USER IF NOT EXISTS 'repuser'@'%' IDENTIFIED BY 'reppass';
-GRANT ALL PRIVILEGES ON daily_report_system.* TO 'repuser'@'%';
+CREATE DATABASE daily_report_system CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+CREATE USER 'repuser'@'%' IDENTIFIED BY 'reppass';
+GRANT ALL PRIVILEGES ON *.* TO 'repuser'@'%';
